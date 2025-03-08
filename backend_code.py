67821c830a0fa630ece0247b6bac4d287aaa1ac8
Ashish Singh <138526056+rj-ash@users.noreply.github.com>
2025-03-08 07:55:44 +0530

    Add files via upload

--- backend_code.py
+++ backend_code.py
@@ -3,6 +3,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
 import pypdf
 import google.generativeai as genai 
@@ -15,8 +17,14 @@ def get_linkedin_details(urls, username, password):
     """Modified to accept credentials as arguments"""
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome()
+    chrome_options.binary_location = '/usr/bin/chromium-browser'
+
+    # Configure driver with Service object
+    service = Service(executable_path='/usr/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     
     try:
         # Login process using provided credentials
--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
+
+
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y \
     chromium-browser \
     chromium-chromedriver \
-    python3-pip \
     libgl1 \
-    libxkbcommon-x11-0
+    libxkbcommon-x11-0 \
+    libxcb-icccm4 \
+    python3-pip \
+    libxcb-image0 \
+    libxcb-keysyms1 \
+    libxcb-randr0 \
+    libxcb-render-util0 \
+    libxcb-xinerama0 \
+    libxcb-xinput0 \
+    libxcb-xfixes0
+sudo ln -s /usr/bin/chromium-browser /usr/bin/chrome
+sudo ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver
